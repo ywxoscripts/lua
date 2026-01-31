18219c37439e042d90f62944a22bd8c7c6074442
ywxo <ywxode@gmail.com>
2026-01-31 20:11:53 +0100

    Update 1489026993.lua

--- 1489026993.lua
+++ 1489026993.lua
@@ -1,1 +1,2 @@
+-- I am aware that you can see the previous versions having no key system here, but the latest features will only be accessible with the key system.
 loadstring(game:HttpGet("https://pastefy.app/qcHi3xbp/raw"))()
